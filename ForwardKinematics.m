f4004f28b30c2b573b0b6e6ed124fcf42d71978f
akowluri <akowluri1@hotmail.com>
2018-01-25 12:15:45 -0800

    Update ForwardKinematics.m

--- ForwardKinematics.m
+++ ForwardKinematics.m
@@ -1,3 +1,5 @@
+#Forward Kinematics Transformation Matrix
+
 function FK=ForwardKinematics(theta)
     FK=rotationZ(theta(1))*translationX(3)*rotationZ(theta(2))*translationX(6)*translationZ(0.5)*translationX(2)*rotationZ(theta(4));
 end
